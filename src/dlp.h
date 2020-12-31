ef8d2925f94245ecc60cecea7a095fae04b6a95a
Hans Jerry Illikainen <hji@dyntopia.com>
2020-12-31 21:54:30 +0000

    dlp: add static asserts for glib typedefs

--- src/dlp.h
+++ src/dlp.h
@@ -8,6 +8,9 @@
 #define DLP_H
 
 #include <limits.h>
+#include <stdint.h>
+
+#include <glib.h>
 
 #define DLP_BUFSIZ MIN(8192, MIN(SIZE_MAX, SSIZE_MAX))
 
@@ -18,6 +21,9 @@
 #    define DLP_NODISCARD __attribute__((warn_unused_result))
 #    define DLP_CONSTRUCTOR __attribute__((constructor))
 #    define DLP_DESTRUCTOR __attribute__((destructor))
+#    define DLP_TYPE_COMPAT_P(x, y) __builtin_types_compatible_p(x, y)
+#    define DLP_ASSERT_TYPE_COMPAT(x, y)                                       \
+        _Static_assert(DLP_TYPE_COMPAT_P(x, y), "incompatible types")
 #else
 #    define DLP_MALLOC
 #    define DLP_ALLOC_SIZE(n)
@@ -25,6 +31,10 @@
 #    define DLP_NODISCARD
 #    define DLP_CONSTRUCTOR _Static_assert(false, "fix DLP_CONSTRUCTOR");
 #    define DLP_DESTRUCTOR _Static_assert(false, "fix DLP_DESTRUCTOR");
+#    define DLP_TYPE_COMPAT_P(x, y)                                            \
+        (sizeof(x) == sizeof(y) && _Alignof(x) == _Alignof(y))
+#    define DLP_ASSERT_TYPE_COMPAT(x, y)                                       \
+        _Static_assert(DLP_TYPE_COMPAT_P(x, y), "incompatible types")
 #endif
 
 #if defined(__clang__)
@@ -39,5 +49,30 @@
 #else
 #    define DLP_DISCARD
 #endif
+
+DLP_ASSERT_TYPE_COMPAT(gchar, char);
+DLP_ASSERT_TYPE_COMPAT(guchar, unsigned char);
+DLP_ASSERT_TYPE_COMPAT(gshort, short);
+DLP_ASSERT_TYPE_COMPAT(gushort, unsigned short);
+DLP_ASSERT_TYPE_COMPAT(gint, int);
+DLP_ASSERT_TYPE_COMPAT(gint8, int8_t);
+DLP_ASSERT_TYPE_COMPAT(gint16, int16_t);
+DLP_ASSERT_TYPE_COMPAT(gint32, int32_t);
+DLP_ASSERT_TYPE_COMPAT(gint64, int64_t);
+DLP_ASSERT_TYPE_COMPAT(gintptr, intptr_t);
+DLP_ASSERT_TYPE_COMPAT(guint, unsigned int);
+DLP_ASSERT_TYPE_COMPAT(guint8, uint8_t);
+DLP_ASSERT_TYPE_COMPAT(guint16, uint16_t);
+DLP_ASSERT_TYPE_COMPAT(guint32, uint32_t);
+DLP_ASSERT_TYPE_COMPAT(guint64, uint64_t);
+DLP_ASSERT_TYPE_COMPAT(guintptr, uintptr_t);
+DLP_ASSERT_TYPE_COMPAT(glong, long);
+DLP_ASSERT_TYPE_COMPAT(gulong, unsigned long);
+DLP_ASSERT_TYPE_COMPAT(gsize, size_t);
+DLP_ASSERT_TYPE_COMPAT(gssize, ssize_t);
+DLP_ASSERT_TYPE_COMPAT(gfloat, float);
+DLP_ASSERT_TYPE_COMPAT(gdouble, double);
+DLP_ASSERT_TYPE_COMPAT(gpointer, void *);
+DLP_ASSERT_TYPE_COMPAT(gconstpointer, const void *);
 
 #endif /* DLP_H */
